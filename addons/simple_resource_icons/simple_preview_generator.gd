e79aa453b6d54fd68f9ef6a207229788beeec904
cameron <cameron.morrow@radar.com>
2025-10-18 09:52:10 -0400

    updated signature, added texture2D handling

--- addons/simple_resource_icons/simple_preview_generator.gd
+++ addons/simple_resource_icons/simple_preview_generator.gd
@@ -6,19 +6,20 @@ func _can_generate_small_preview():
 func _handles(type):
 	return type == "Resource"
 
-func _generate(resource, size):
-	var img : Image = Image.new()
-	
-	# The _get_icon method must return an [Image].
+func _generate(resource, size, _metadata = {}):
+	var img: Image = Image.new()
+
 	# The resource script must be a @tool script.
 	if resource.has_method(&"_get_icon"):
 		var icon = resource.call(&"_get_icon")
 		if icon:
-			img = icon
-			img.resize(size.x, size.y, Image.INTERPOLATE_NEAREST)
-	if &"icon" in resource:
+			if icon is Texture2D:
+				img = icon.get_image()
+			else:
+				img = icon
+	elif &"icon" in resource:
 		if resource.icon:
 			img = resource.icon.get_image()
-			img.resize(size.x, size.y, Image.INTERPOLATE_NEAREST)
-	
+	img.resize(size.x, size.y, Image.INTERPOLATE_NEAREST)
+
 	return ImageTexture.create_from_image(img)
